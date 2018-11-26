c613756b6522f5d0abbfbbcf1d03432a521ed5f9
Danielle333 <42211357+Danielle333@users.noreply.github.com>
2018-11-26 09:54:42 -0600

    Adding gallery.pug, nav.pug, layout.pug, studioghibli.pug, images, styling.css, improving main, and improving moviepicker.pug

--- main.js
+++ main.js
@@ -51,6 +51,19 @@ app.get('/addMovie', function(req, res) {
     movieModule.addToFavorites(req.query.movie);
 });
 
+
+app.get('/', function(req, res) {
+	res.render('moviepicker');
+});
+
+app.get('/gallery', function(req, res) {
+	res.render('gallery');
+});
+
+app.get('/studioghibli', function(req, res) {
+	res.render('studioghibli');
+});
+
 const server = app.listen(3000, function() {
 	console.log(`Server started on port ${server.address().port}`);
 });
