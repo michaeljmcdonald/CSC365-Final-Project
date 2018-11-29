951c31c7fc0bc6314d0618712f357c9e704e9945
Michael McDonald <McDonald730@live.missouristate.edu>
2018-11-29 16:12:24 -0600

    :x!

--- main.js
+++ main.js
@@ -3,6 +3,34 @@ const express = require('express'),
 	app = express(),
 	request = require('request-promise'),
     movieModule = require('./modules/MovieManager');
+    express = require('express');
+    passport = require('passport');
+    Strategy = require('passport-twitter').Strategy;
+
+var trustProxy = false;
+if (process.env.DYNO) {
+  
+  trustProxy = true;
+}
+
+passport.use(new Strategy({
+    consumerKey: process.env['TWITTER_CONSUMER_KEY'],
+    consumerSecret: process.env['TWITTER_CONSUMER_SECRET'],
+    callbackURL: '/oauth/callback/twitter.com',
+    proxy: trustProxy
+  },
+  function(token, tokenSecret, profile, cb) {
+    
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -15,6 +43,45 @@ app.use(
 		extended: true
 	})
 ); // for parsing application/x-www-form-urlencoded
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    console.log('ENV');
+    console.log(process.env);
+    console.log('Headers:');
+    console.log(req.headers)
+    res.render('login');
+  });
+
+app.get('/login/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/oauth/callback/twitter.com',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
 
 app.get('/', function(req, res) {   
     let allMovies = null;
@@ -50,7 +117,6 @@ app.get('/addMovie', function(req, res) {
     console.log(req.query.movie)
     movieModule.addToFavorites(req.query.movie);
 });
-
 
 app.get('/', function(req, res) {
 	res.render('moviepicker');
